c8495d3016c0253ba82151fde0fa1c9eebc23a8a
Zastian Pretorius <zastian00@gmail.com>
2023-10-29 21:06:52 +0000

    Added GetUsers functions

--- api/api.go
+++ api/api.go
@@ -28,4 +28,19 @@ func SetupRoutes(r *gin.Engine) {
 			"message": "test passed",
 		})
 	})
+	// /getUsers returns a json array of all users in the database
+	r.GET("/getUsers", func(c *gin.Context) {
+		//GetUsers returns a slice of person structs
+		users := db.GetUsers()
+		userJson := []gin.H{}
+		for _, user := range users {
+			userJson = append(userJson, gin.H{
+				"id":        user.Id,
+				"firstName": user.FirstName,
+				"lastName":  user.LastName,
+			})
+		}
+		c.JSON(http.StatusOK, userJson)
+
+	})
 }
--- database/db.go
+++ database/db.go
@@ -21,4 +21,32 @@ func Run(firstName string, lastName string) {
 		rows.Scan(&id, &firstName, &lastName)
 		fmt.Println(strconv.Itoa(id) + ": " + firstName + " " + lastName)
 	}
+
+}
+
+type Person struct {
+	Id        int
+	FirstName string
+	LastName  string
+}
+
+func GetUsers() []Person {
+	db, _ := sql.Open("sqlite3", "./srs.db")
+	statement, _ := db.Prepare("CREATE TABLE if NOT EXISTS balls(id INTEGER PRIMARY KEY, firstName TEXT, lastName TEXT)")
+	statement.Exec()
+	defer db.Close()
+	rows, _ := db.Query("SELECT * FROM balls")
+	personSlice := []Person{}
+	var id int
+	var firstName string
+	var lastName string
+	for rows.Next() {
+		rows.Scan(&id, &firstName, &lastName)
+		var dbUser Person
+		dbUser.Id = id
+		dbUser.FirstName = firstName
+		dbUser.LastName = lastName
+		personSlice = append(personSlice, dbUser)
+	}
+	return personSlice
 }
